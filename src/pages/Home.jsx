0afc6bdd0952fd842d193a0b71028647c58c4578
michelldenjoy <michelldenjoy@gmail.com>
2025-11-24 09:35:44 +0100

    deploy

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,11 @@
 import QuandumParticles from "../components/3d/QuandumParticles";
-import Certificaciones from "../pages/SobreQuandum/Certificaciones";
+
 
 export default function Home() {
   return (
     <section>
       <QuandumParticles />
-      <Certificaciones />
+      
     </section>
   );
 }
