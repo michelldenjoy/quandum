b94adece50229bd886b643d0887dbedc7f9693fa
michelldenjoy <michelldenjoy@gmail.com>
2025-12-02 17:53:33 +0100

    overflow error

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -103,7 +103,7 @@ export default function Contacto() {
   ];
 
   return (
-    <div className="relative min-h-screen overflow-hidden">
+    <div className="fixed inset-0 -z-10">
       <StarfieldNebula />
 
       {/* HERO */}
