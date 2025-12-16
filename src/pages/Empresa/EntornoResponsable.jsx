65167ee15e7f89f06739f3416a8456ed8fba3fcc
michelldenjoy <michelldenjoy@gmail.com>
2025-12-16 09:54:33 +0100

    upload v18

--- src/pages/Empresa/EntornoResponsable.jsx
+++ src/pages/Empresa/EntornoResponsable.jsx
@@ -6,7 +6,7 @@ export default function EntornoResponsable() {
   return (
     <section>
         <div className="bg-black h-max py-11 mt-20 w-full">
-          <h1>Entorno Responsable Quandum aerospace </h1>
+          <h1>Entorno Responsable Quandum </h1>
         </div>
     </section>
   );
