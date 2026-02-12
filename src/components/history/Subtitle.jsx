3e6bde634c5074bbb9a91c31497d52349ae862df
michelldenjoy <michelldenjoy@gmail.com>
2026-02-12 12:57:51 +0100

    webkit

--- src/components/history/Subtitle.jsx
+++ src/components/history/Subtitle.jsx
@@ -11,11 +11,6 @@ const container = {
   },
 };
 
-const word = {
-  hidden: {},
-  visible: {},
-};
-
 const letter = {
   hidden: { opacity: 0, y: 40 },
   visible: {
@@ -35,24 +30,22 @@ export default function Subtitle() {
       className="text-center mb-24"
     >
       <h2 className="text-5xl pt-24 md:text-6xl lg:text-7xl font-light tracking-tight leading-tight">
-        <span className="bg-gradient-to-r from-slate-300 via-slate-400 to-slate-500 bg-clip-text -webkit-bg-clip-text text-transparent">
-          {text.split(" ").map((wordText, wordIndex) => (
-            <span
-              key={wordIndex}
-              className="inline-block whitespace-nowrap mr-3"
-            >
-              {wordText.split("").map((char, charIndex) => (
-                <motion.span
-                  key={charIndex}
-                  variants={letter}
-                  className="inline-block"
-                >
-                  {char}
-                </motion.span>
-              ))}
-            </span>
-          ))}
-        </span>
+        {text.split(" ").map((wordText, wordIndex) => (
+          <span
+            key={wordIndex}
+            className="inline-block whitespace-nowrap mr-3"
+          >
+            {wordText.split("").map((char, charIndex) => (
+              <motion.span
+                key={charIndex}
+                variants={letter}
+                className="inline-block bg-gradient-to-r from-slate-300 via-slate-400 to-slate-500 bg-clip-text text-transparent"
+              >
+                {char}
+              </motion.span>
+            ))}
+          </span>
+        ))}
       </h2>
     </motion.div>
   );
